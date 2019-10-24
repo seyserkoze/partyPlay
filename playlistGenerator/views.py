42668085a9404811785b90a24a90c132334ba3c9
seyserkoze <skhazanc@andrew.cmu.edu>
2019-10-23 23:20:51 -0700

    templates work- config sortted

--- playlistGenerator/views.py
+++ playlistGenerator/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<div> fuck </div>")
+     return render(request, 'spotifyAuth.html', {}, content_type='text/html')
